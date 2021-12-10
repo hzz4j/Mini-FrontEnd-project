c1181a4864e84a9b47eb0601218c9854f73dd654
静默 <cau1403090523@gmail.com>
2021-12-10 16:44:31 +0800

    add event listener

--- 01 mini-website/dist/index.js
+++ 01 mini-website/dist/index.js
@@ -1,2 +1,11 @@
 "use strict";
-console.log("hello world");
+let modalElement = document.querySelector(".modal");
+let backdropElement = document.querySelector(".backdrop");
+let buttons = document.querySelectorAll(".plan button");
+console.log(modalElement);
+buttons.forEach((node) => {
+    node.addEventListener("click", (e) => {
+        modalElement.style.display = "block";
+        backdropElement.style.display = "block";
+    });
+});
--- 01 mini-website/src/index.ts
+++ 01 mini-website/src/index.ts
@@ -1,1 +1,12 @@
-console.log("hello world");
+let modalElement = document.querySelector(".modal")! as HTMLDivElement;
+let backdropElement = document.querySelector(".backdrop")! as HTMLDivElement;
+let buttons: NodeListOf<Element> = document.querySelectorAll(".plan button");
+
+console.log(modalElement);
+
+buttons.forEach((node) => {
+  node.addEventListener("click", (e) => {
+    modalElement.style.display = "block";
+    backdropElement.style.display = "block";
+  });
+});
