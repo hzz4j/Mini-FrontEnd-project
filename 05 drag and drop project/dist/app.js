0c61d896b54d88c4793397f022e27d98450a8886
静默 <cau1403090523@gmail.com>
2021-12-17 13:05:11 +0800

    use decorator to bind this

--- 05 drag and drop project/dist/app.js
+++ 05 drag and drop project/dist/app.js
@@ -1,4 +1,24 @@
 "use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+// autobind
+function AutoBind(target, name, descriptor) {
+    console.log(name);
+    console.log(descriptor.value);
+    const originMethod = descriptor.value;
+    const newDescriptor = {
+        configurable: true,
+        get() {
+            const boundFn = originMethod.bind(this);
+            return boundFn;
+        },
+    };
+    return newDescriptor;
+}
 class ProjectInput {
     constructor() {
         this.templateElement = document.getElementById("project-input");
@@ -25,7 +45,11 @@ class ProjectInput {
         console.log(title);
     }
     configure() {
-        this.formElement.addEventListener("submit", this.submitHandler.bind(this));
+        // this.formElement.addEventListener("submit", this.submitHandler.bind(this));
+        this.formElement.addEventListener("submit", this.submitHandler);
     }
 }
+__decorate([
+    AutoBind
+], ProjectInput.prototype, "submitHandler", null);
 const prjInput = new ProjectInput();
--- 05 drag and drop project/src/app.ts
+++ 05 drag and drop project/src/app.ts
@@ -1,10 +1,26 @@
+// autobind
+function AutoBind(target: any, name: string, descriptor: PropertyDescriptor) {
+  console.log(name);
+  console.log(descriptor.value);
+  const originMethod = descriptor.value;
+  const newDescriptor: PropertyDescriptor = {
+    configurable: true,
+    get() {
+      const boundFn = originMethod.bind(this);
+      return boundFn;
+    },
+  };
+
+  return newDescriptor;
+}
+
 class ProjectInput {
   templateElement: HTMLTemplateElement;
   appElement: HTMLDivElement;
   formElement: HTMLFormElement;
   titleElement: HTMLInputElement;
   descriptionElement: HTMLTextAreaElement;
-  peopleElement:HTMLInputElement;
+  peopleElement: HTMLInputElement;
 
   constructor() {
     this.templateElement = document.getElementById(
@@ -19,9 +35,15 @@ class ProjectInput {
     this.formElement = importNode.firstElementChild as HTMLFormElement;
     this.formElement.id = "user-input";
 
-    this.titleElement = this.formElement.querySelector("#title") as HTMLInputElement;
-    this.descriptionElement = this.formElement.querySelector("#description") as HTMLTextAreaElement;
-    this.peopleElement = this.formElement.querySelector("#people") as HTMLInputElement;
+    this.titleElement = this.formElement.querySelector(
+      "#title"
+    ) as HTMLInputElement;
+    this.descriptionElement = this.formElement.querySelector(
+      "#description"
+    ) as HTMLTextAreaElement;
+    this.peopleElement = this.formElement.querySelector(
+      "#people"
+    ) as HTMLInputElement;
 
     this.attach();
     this.configure();
@@ -34,15 +56,15 @@ class ProjectInput {
     this.appElement.insertAdjacentElement("afterbegin", this.formElement);
   }
 
-  private submitHandler(event: Event){
-      event.preventDefault();
-      const title = this.titleElement.value;
-      console.log(title);
-      
-
+  @AutoBind
+  private submitHandler(event: Event) {
+    event.preventDefault();
+    const title = this.titleElement.value;
+    console.log(title);
   }
-  private configure(){
-      this.formElement.addEventListener("submit",this.submitHandler.bind(this))
+  private configure() {
+    // this.formElement.addEventListener("submit", this.submitHandler.bind(this));
+    this.formElement.addEventListener("submit", this.submitHandler);
   }
 }
 
