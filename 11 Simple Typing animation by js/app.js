ed4a6ace82a20944315f9d03e6bdbde9689f48c6
静默 <cau1403090523@gmail.com>
2022-07-16 21:59:24 +0800

    我打碎了夕阳

---
+++ 11 Simple Typing animation by js/app.js
@@ -0,0 +1,40 @@
+const words = ['我打碎了夕阳','洒在你身上','你满是圣光','我黯然退场']
+const linesDiv = document.querySelectorAll(".line")
+
+const word = document.getElementById('word')
+// ,'洒在你身上','你满是圣光','我黯然退场'
+let arrayIndex = 0;
+let charIndex = 1;
+let allText = ''
+
+let currentLine = linesDiv[arrayIndex]
+
+let refresh = setInterval(() => {
+    let content = words[arrayIndex].substring(0,charIndex);
+    currentLine.innerText = content
+
+    if(content.length === words[arrayIndex].length){
+        allText += content;
+        if(arrayIndex === words.length-1){
+            linesDiv.forEach(div => div.innerText='')
+            charIndex = 0;
+            arrayIndex = 0;
+            // clearInterval(refresh)
+        }else{
+            // next line
+            arrayIndex++
+            charIndex = 0;
+        }
+    }
+    currentLine = linesDiv[arrayIndex]
+    charIndex++;
+    
+}, 300);
+
+
+function createDiv(){
+    const div = document.createElement('div')
+    div.classList.add('line')
+    return div;
+}
+
