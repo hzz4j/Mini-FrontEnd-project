f24f8446cc2cbbfb56cacd755a9046b874072255
静默 <cau1403090523@gmail.com>
2022-07-18 02:26:46 +0800

    Random image feed

---
+++ 14 Random Image Feed/app.js
@@ -0,0 +1,24 @@
+const container = document.querySelector(".container")
+const BASIC_API = "https://source.unsplash.com/random/"
+let rows = 10
+
+// load images
+for(i = 0;i < rows * 3;i++){
+    const img = document.createElement('img')
+    img.src = generateRandomLink()
+    container.appendChild(img)
+}
+
+console.log(generateRandomLink())
+
+function getRandomNr(){
+    return Math.floor(Math.random() * 10) + 300
+}
+
+function getRandomSize(){
+    return `${getRandomNr()}x${getRandomNr()}`
+}
+
+function generateRandomLink(){
+    return BASIC_API + getRandomSize()
+}
