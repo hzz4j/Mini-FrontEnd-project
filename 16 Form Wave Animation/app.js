9f41dbea5f8be9156b74fe6bdaf1c84b68f13d0f
静默 <cau1403090523@gmail.com>
2022-07-18 21:30:09 +0800

    Update app.js

--- 16 Form Wave Animation/app.js
+++ 16 Form Wave Animation/app.js
@@ -1,8 +1,20 @@
 const labels = document.querySelectorAll('.form-control label')
 
+/**
+ 将<label>Email</lable>变成
+ <label>
+    <span style="transition-delay:0ms">E</span>
+    <span style="transition-delay:50ms">a</span>
+    <span style="transition-delay:150ms">m</span>
+    <span style="transition-delay:200ms">i</span>
+    <span style="transition-delay:250ms">l</span>
+ </label>
+ */
+
 labels.forEach(label => {
     label.innerHTML = label.innerText
         .split('')
         .map((letter,idx) => `<span style='transition-delay:${idx * 50}ms'>${letter}</span>`)
         .join('')
 })
+
