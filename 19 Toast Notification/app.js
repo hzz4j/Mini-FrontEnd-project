70571a1a2b0a5b7e499b32802764366dee6107f0
静默 <cau1403090523@gmail.com>
2022-07-19 04:06:41 +0800

    finish toast notification

---
+++ 19 Toast Notification/app.js
@@ -0,0 +1,42 @@
+const toastsContainer = document.getElementById("toasts")
+const notifiBtn = document.getElementById('notification')
+
+const messages = [
+    'Message One',
+    'Message Two',
+    'Message Three',
+    'Message Four',
+    'Message Five'
+]
+
+const types = [
+    'info',
+    'error',
+    'success'
+]
+
+notifiBtn.addEventListener('click',() => {
+    showNotification()
+})
+
+function showNotification(message = '',type = ''){
+    const div = document.createElement('div')
+    div.classList.add('toast')
+    div.classList.add(type ? type : getRandomType())
+
+    div.innerText = message ? message : getRandomMsg()
+    toastsContainer.appendChild(div)
+
+    setTimeout(() => {
+        div.remove()
+    }, 3000);
+}
+
+
+function getRandomMsg(){
+    return messages[Math.floor(Math.random() * messages.length)]
+}
+
+function getRandomType(){
+    return types[Math.floor(Math.random() * types.length)]
+}
