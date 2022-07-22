1faee2e1d33699169d3d96101ed47cc9a16d9c5d
静默 <cau1403090523@gmail.com>
2022-07-22 17:52:52 +0800

    finish the basic image carousel

--- 26 Image Carousel 1/src/app.ts
+++ 26 Image Carousel 1/src/app.ts
@@ -1,13 +1,24 @@
 const imgsContainer = document.getElementById('imgs-container')! as HTMLElement
 const carousel = document.getElementById('carousel')! as HTMLElement
 const imgs = imgsContainer.querySelectorAll('img')! as NodeListOf<HTMLElement>
+const dots = document.querySelectorAll('.dots li')! as NodeListOf<HTMLElement>
+const prev = document.getElementById('prev')! as HTMLElement
+const next = document.getElementById('next')! as HTMLElement
+
 
 let idx = 0
 let size = computeSize()
-
 let interval = setInterval(forward,2000)
 
 
+
+prev.addEventListener('click',()=>{
+    resetIntervalAndCallFunc(back)
+})
+
+next.addEventListener('click',()=>{
+    resetIntervalAndCallFunc(forward)
+})
 
 imgsContainer.addEventListener('transitionend',()=>{
     console.log("transitionend");
@@ -16,12 +27,25 @@ imgsContainer.addEventListener('transitionend',()=>{
 
 function forward(){
     idx++    
+    changeImage()
+}
+
+function back(){
+    idx--
+    changeImage()
+}
+
+
+function changeImage(){
     if(idx > imgs.length - 1){
         // first limit counter to prevent fast-change bugs
         // 解决快速回退的问题
        
         idx = 0
+    }else if(idx<0){
+        idx = imgs.length - 1
     }
+    hightDots()
     imgsContainer.style.transform = `translateX(${-size * idx}px)`
 }
 
@@ -31,6 +55,21 @@ function computeSize():number{
     return +width.substring(0,width.length-2)
 }
 
+
+
+function hightDots(){
+    // remove all active then add active class
+    const activeCls = 'active'
+    dots.forEach(dot => dot.classList.remove(activeCls))
+    dots[idx].classList.add(activeCls)
+}
+
+
+function resetIntervalAndCallFunc(func: Function){
+    clearInterval(interval)
+    func()
+    interval = setInterval(forward,2000)
+}
 
 // loop
 // https://codepen.io/jonathan-asbell/pen/QWgampv
