011e17f16644c3dbf43508edc1f5e01f324ed01d
静默 <cau1403090523@gmail.com>
2022-07-23 19:05:23 +0800

    finish vue 框架引入

--- 28 front end project change/04-1 step four/src/app.ts
+++ 28 front end project change/04-1 step four/src/app.ts
@@ -1,16 +1,43 @@
-
-
 /**
  * Vue will automatically use the container's innerHTML as the template 
  * if the root component does not already have a template option.
  */
 // root component
 import { createApp } from 'vue'
+import './app.scss'
+import TodoItem from '@/types/TodoItems'
 
-createApp({
+
+
+const app = createApp({
   data() {
     return {
-      count: 0
+      todoItems: [{
+        id: Date.now().toString(),
+        title: 'Learn front end knowledge',
+        completed: false
+      }] as TodoItem[]
+    }
+  },
+  methods: {
+    addTodo(e:Event){
+      const inputEl = e.target as HTMLInputElement
+      const todoItem:TodoItem = {
+        id: Date.now().toString(),
+        title: inputEl.value,
+        completed: false
+      }
+      this.todoItems.push(todoItem)
+      inputEl.value = ''
+    },
+    deleteTodoItem(todoItem:TodoItem){
+      console.log(todoItem);
+      if(todoItem){
+        this.todoItems.splice(this.todoItems.indexOf(todoItem),1)
+      }
     }
   }
-}).mount('#app')
+
+})
+
+app.mount('#app')
