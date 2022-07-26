e457bf7dcf2e12ef94fcb103def878a7708d5866
静默 <cau1403090523@gmail.com>
2022-07-26 16:19:35 +0800

    update

---
+++ 33 Increment Counter/vite-project/src/ts/fontawesome-icons.ts
@@ -0,0 +1,9 @@
+import { library } from "@fortawesome/fontawesome-svg-core";
+// import {fab } from "@fortawesome/free-brands-svg-icons"
+import {faTiktok,faWeibo,faBilibili } from "@fortawesome/free-brands-svg-icons"
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+
+library.add(faTiktok,faWeibo,faBilibili)
+
+export default FontAwesomeIcon
