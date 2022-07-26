55b21a71dd06a985cc75e591e39d451d44003540
静默 <cau1403090523@gmail.com>
2022-07-27 02:24:09 +0800

    add all

--- 35 Drag N Drop/vanilla/src/main.ts
+++ 35 Drag N Drop/vanilla/src/main.ts
@@ -4,8 +4,6 @@ const fill = document.querySelector(".fill")!
 const empties = document.querySelectorAll(".empty")!
 
 
-setTimeout(()=>{empties[2].appendChild(fill)},3000)
-
 
 empties.forEach(empty => {
   empty.addEventListener("dragenter",dragEnter)
@@ -20,15 +18,24 @@ fill.addEventListener("dragend",dragEnd)
 
 
 function dragStart(this:HTMLElement){
+  this.classList.add("hold")
+
+  // 拖拽之后立刻去除样式，但是样式在拖拽的元素中仍然生效
+  setTimeout(() => {
+    this.className = ""
+  }, 1);
+  
   console.log(this.getAttribute("name-attr"),"drag start")
 }
 
 function dragEnd(this:HTMLElement){
+  this.classList.add("fill")
   console.log(this.getAttribute("name-attr"),"drag end");
 }
 
 
 function dragEnter(this:HTMLElement){
+  this.classList.add('over')
   console.log("drag enter to ",this.getAttribute("name-attr"))
 }
 
@@ -38,10 +45,12 @@ function dragOver(event:Event){
 }
 
 function dragLeave(this:HTMLElement){
+  this.classList.remove('over')
   console.log("drag leave from ",this.getAttribute("name-attr"))
 }
 
 function dragDrop(this:HTMLElement){
+  this.classList.remove('over')
   console.log("drag drop in ",this.getAttribute("name-attr"));
-  this.appendChild(fill)
+  this.appendChild(fill) // 核心
 }
