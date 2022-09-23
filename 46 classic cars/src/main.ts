19552e377ead569a6e4ef1d5240e6cdc75e27307
静默 <cau1403090523@gmail.com>
2022-09-24 01:04:16 +0800

    finished project

--- 46 classic cars/src/main.ts
+++ 46 classic cars/src/main.ts
@@ -2,6 +2,7 @@ import './style.scss'
 
 const menu = document.querySelector(".menu")!
 const wrappers = document.querySelectorAll(".wrapper")!
+const videos = document.querySelectorAll(".video")! as NodeListOf<HTMLVideoElement>
 
 menu.addEventListener("click",()=>{
   const els = document.querySelectorAll(".target")!
@@ -14,3 +15,13 @@ wrappers.forEach(wrapper => [
     els.forEach(el => el.classList.remove('change'))
   })
 ])
+
+videos.forEach(video => {
+  video.addEventListener("mouseover",()=>{
+    video.play()
+  })
+
+  video.addEventListener("mouseout",()=>{
+    video.pause()
+  })
+})
