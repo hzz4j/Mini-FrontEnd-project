187ebbed9004b02db7c0c6f16c12e4d4203986d2
静默 <cau1403090523@gmail.com>
2022-09-25 00:37:45 +0800

    add axios

---
+++ 47 browser extension/src/index.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+// form fields
+const form = document.querySelector('.form-data');
+const region = document.querySelector('.region-name');
+
+const apiKey = document.querySelector('.api-key');
+
+// results
+const errors = document.querySelector('.errors');
+const loading = document.querySelector('.loading');
+const results = document.querySelector('.result-container');
+const usage = document.querySelector('.carbon-usage');
+const fossilfuel = document.querySelector('.fossil-fuel');
+const myregion = document.querySelector('.my-region');
+const clearBtn = document.querySelector('.clear-btn');
