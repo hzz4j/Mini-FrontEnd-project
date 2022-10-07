0cb1a071736630fe70bcca2ce8e5f694ad073552
静默 <cau1403090523@gmail.com>
2022-10-07 21:08:58 +0800

    finished color switch

---
+++ 49 color switch/src/main.ts
@@ -0,0 +1,21 @@
+import "./style.scss";
+
+const colors = document.querySelectorAll(".color")!;
+const btn = document.getElementById("btn")! as HTMLInputElement;
+const txt = document.querySelector(".txt")! as HTMLInputElement;
+colors.forEach((color) => color.addEventListener("click", changeColor));
+
+function changeColor(event: Event) {
+  const target = event.target as HTMLElement;
+  // get color
+  if (target) {
+    const color = getComputedStyle(target).getPropertyValue("background-color");
+    document.body.style.backgroundColor = color;
+    txt.style.borderLeftColor = color;
+    btn.style.backgroundColor = color;
+
+    // active color
+    colors.forEach((color) => color.classList.remove("active"));
+    target.classList.add("active");
+  }
+}
